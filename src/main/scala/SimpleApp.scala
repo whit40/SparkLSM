3699e01683dad58e4de4114a82638b09c232f7e2
Andrew Whitaker <acw123103478@gmail.com>
2022-07-28 22:30:03 -0400

    almost done, just need to figure out issue with constructing partition bounds

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -12,7 +12,6 @@ object SimpleApp {
   val maxSize = 2
   val levelCount = 3
   val numPartitions = 2
-  
 
   var level0 = scala.collection.mutable.ArrayBuffer.empty[Tuple2[Int,Int]]
   //val levelArray = scala.collection.mutable.ArrayBuffer.empty[RDD[Tuple2[Int,Int]]]
@@ -25,10 +24,6 @@ object SimpleApp {
   //var level1 = spark1.sparkContext.emptyRDD[Tuple2[Int, Int]]
   var level2 = spark1.sparkContext.emptyRDD[Tuple2[Int, Int]]
   
-  //def partFilter(a:Tuple2(Int, Int)) : Int = {
-    
-  //}
-  
   // Insert/Delete function here (~5 lines)
   def modifyLSM(value: Tuple2[Int, Int]) : Boolean = {
     val spark = SparkSession.builder().getOrCreate()
@@ -38,10 +33,10 @@ object SimpleApp {
     // Check if we need to merge and merge if needed
     if (level0.length > maxSize){
       // Call merge
-      println("call merge")
+      //println("call merge")
       mergePacked(0,1)
     } else {
-      println("no merge needed, size is: " + level0.length)
+      //println("no merge needed, size is: " + level0.length)
     }
     return true
   }
@@ -63,11 +58,12 @@ object SimpleApp {
   
   val levelArray2 = scala.collection.mutable.ArrayBuffer.empty[RDD[Array[Tuple2[Int,Int]]]]
   val partArray = scala.collection.mutable.ArrayBuffer.empty[RangePartitioner[Int, Int]]
+  val rangeArray = scala.collection.mutable.ArrayBuffer.empty[scala.collection.mutable.ArrayBuffer[Tuple2[Int, Int]]]
   
   for (l <- 0 to levelCount){
-    println("blah")
     levelArray2 += spark1.sparkContext.emptyRDD[Array[Tuple2[Int, Int]]]
     partArray += new RangePartitioner(numPartitions, spark1.sparkContext.emptyRDD[Tuple2[Int, Int]])
+    rangeArray += scala.collection.mutable.ArrayBuffer.empty[Tuple2[Int,Int]]
   }
   
   def mergePacked(levelId1: Int, levelId2: Int) : Unit = {
@@ -88,15 +84,18 @@ object SimpleApp {
   
     // Flatmap and Merge levels with the parameter Ids
     var flatUnion = flatlevel1 ++ flatlevel2
-    //flatUnion = flatUnion.sortByKey()
     levelArray2(levelId1) = spark1.sparkContext.emptyRDD[Array[Tuple2[Int, Int]]]
     partArray(levelId1) = new RangePartitioner(numPartitions, spark1.sparkContext.emptyRDD[Tuple2[Int, Int]])
+    println("resetting level ranges for " + levelId1 + " and " + levelId2)
+    //rangeArray(levelId1) = scala.collection.mutable.ArrayBuffer.empty[Tuple2[Int,Int]]
+    //rangeArray(levelId2) = scala.collection.mutable.ArrayBuffer.empty[Tuple2[Int,Int]]
+    rangeArray(levelId1).clear
+    rangeArray(levelId2).clear
     
     // use tombstones to remove appropriate vals
     flatUnion = flatUnion.reduceByKey(tomestoneCalc)
     
     flatUnion = flatUnion.filter(a => a._2 != -2)
-    //flatUnion = flatUnion.sortByKey()
     val flatSize = flatUnion.count
     partArray(levelId2) = new RangePartitioner(numPartitions, flatUnion)
     flatUnion = flatUnion.partitionBy(partArray(levelId2))
@@ -109,10 +108,19 @@ object SimpleApp {
     levelArray2(levelId2) = flatUnion.mapPartitions(iter => {
       var array = iter.toArray
       array = array.sortBy(_._1)
+      val firstKey = array(0)._1
+      val lastKey = array.last._1
+      val boundTuple = (firstKey, lastKey)
+      rangeArray(levelId2) += boundTuple
       Iterator({array})
       }
     )
-    
+    println("array size before sort is " + rangeArray(levelId2).length)
+    rangeArray(levelId2) = rangeArray(levelId2).sortBy(_._1)
+    println("sort results in merge +++++++++++++==")
+    println("array size is " + rangeArray(levelId2).length)
+    rangeArray(levelId2).foreach(println)
+    println("end sort results in merge +++++++++++++==")
     
     if (flatSize > (maxSize*levelId2*2)){
       
@@ -131,6 +139,7 @@ object SimpleApp {
     return true
   }
   
+  // Simple binary search to search final pruned RDD/array
   def binarySearch(array: Array[Tuple2[Int,Int]], target: Int) : Int = {
     var lo = 0
     var hi = array.length - 1
@@ -140,6 +149,32 @@ object SimpleApp {
       
       if (array(mid)._1 == target){
         return array(mid)._2
+        
+      } else if (array(mid)._1 > target){
+        hi = mid - 1 
+        
+      } else {
+        lo = mid + 1
+      }
+    }
+    
+    return -1
+  }
+  
+  // Binary search to find the correct partition in a search. Array Parameter is an Array of 2Tuples, where
+  // the first value is the first key of the partition, and the second value is the last key of the partition.
+  // Returns the partition number where the target would be.
+  def rangeBinarySearch(array: scala.collection.mutable.ArrayBuffer[Tuple2[Int,Int]], target: Int) : Int = {
+    var lo = 0
+    var hi = array.length - 1
+    
+    array.foreach(println)
+    
+    while (lo <= hi){
+      var mid = ((hi - lo) / 2) + lo
+      
+      if (array(mid)._1 <= target && array(mid)._2 >= target){
+        return mid
         
       } else if (array(mid)._1 > target){
         hi = mid - 1 
@@ -162,9 +197,16 @@ object SimpleApp {
     } else {
       partResults += 0
     }
-     
+    println("L3 check")
+    println(rangeArray(3).length)
+    
     for (level <- 1 to levelCount) {
       partResults += partArray(level).getPartition(key)
+      println("doing level " + level + "=====================")
+      println("result from simple part search: "+ partArray(level).getPartition(key))
+      println("result from manual part search: "+ rangeBinarySearch(rangeArray(level), key))
+      println("+=+=+=+")
+      rangeArray(level).foreach(println)
     }
     
     partResults.foreach(println)
@@ -172,9 +214,7 @@ object SimpleApp {
     // use partitionpruningrdd to get partition
     for (level <- 1 to levelCount){
       val prunedRDD = new PartitionPruningRDD(levelArray2(level), (part => {part == partResults(level)}))
-      //val prunedRDD = new PartitionPruningRDD(levelArray(level), id => pruneFunc(id,partResults(level)))
-      //val prunedRDD = new PartitionPruningRDD(levelArray2(level), id => true)
-      //val flatPruned = prunedRDD.flatMap(list => list)
+      
       println("partition results from level " + level  + " have size: " + prunedRDD.count())
       //println(prunedRDD.collect())
       println(prunedRDD.flatMap(list => list).collect.foreach(println))
@@ -224,21 +264,16 @@ object SimpleApp {
     //val partitioner = new RangePartitioner(6, level1)
     
     //println("Partitioner has this many partitions: " + partitioner.numPartitions)
-    
-    //println("Get partition with key 1: " + partitioner.getPartition(1))
-    
+
     //level1.glom().collect().foreach(a => {a.foreach(println);println("=====")})
-    println("trying flatmap ============================")
-    val arrayofArrays = Array(Array(1,2,3,4), Array(5,6,7,8)) 
-    var exRDD = spark1.sparkContext.parallelize(arrayofArrays)
-    exRDD.flatMap(list => list).collect().foreach(println)
+
     
     val initPath = "/home/whit/spark-3.1.3-bin-hadoop3.2/simple_ex/src/main/initialstate.txt"
-    println("printing input")
+    //println("printing input")
     for (line <- Source.fromFile(initPath).getLines){
       val splitTuple = line.split(",")
       modifyLSM((splitTuple(0).toInt, splitTuple(1).toInt))
-      println(line)
+      //println(line)
     }
     
     println("Try inserting into rdd")
@@ -271,6 +306,10 @@ object SimpleApp {
     search(8)
     //println("partition for key 24 is: " +  )
     
+    //val exRanges = Array((1,7),(8,11),(14,20))
+    //println("partition search result is " + rangeBinarySearch(exRanges, 12))
+    
+    rangeArray(1).foreach(println)
     // Use mappartitions to build level 1 index
     //val index = partdata.mapPartitions(x=> (List(x.next._1).iterator)).collect
     //index.foreach(println)
